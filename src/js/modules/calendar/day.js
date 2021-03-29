18ff2a184580617ea11d6dbaff3997c22a67abf0
Ariqun <myhomewalhall@gmail.com>
2021-03-29 19:06:44 +0300

    Add day screen, v0.4.1

--- src/js/modules/calendar/day.js
+++ src/js/modules/calendar/day.js
@@ -12,16 +12,48 @@ export default class Day {
 	}
 
 	centeringNumberOfDay() {
-		const width = document.querySelector('.dateDay').offsetWidth;
-		const width1 = document.querySelector('.dateDay').clientWidth;
-		const height = document.querySelector('.dateDay').offsetHeight;
+		const top = document.querySelector('.dateDay').getBoundingClientRect();
 
-		console.log(width, width1, height);
+		console.log(top);
+	}
+
+	createGraph() {
+		const table = document.createElement('table');
+
+		for (let i = 1; i <= 24; i++) {
+			const tr = document.createElement('tr');
+			const td = document.createElement('td');
+			const time = document.createElement('div');
+				  time.classList.add('time');
+			
+			time.innerHTML = `${addZero(i)}:00`;
+			tr.append(time, td);
+			table.appendChild(tr);
+		}
+
+		function addZero(num) {
+			if (num < 10) {
+				return `0${num}`;
+			} else {
+				return num;
+			}
+		}
+
+		document.querySelector('.day').prepend(table);
+	}
+
+	createListeners() {
+		document.querySelector('.day').addEventListener('scroll', () => {
+			const px = document.querySelector('.day').scrollTop;
+			document.querySelector('.day .dateDay').style.top = `calc(50% - ${200 - px}px)`;
+		});
 	}
 
 
 	init() {
 		this.createDay();
 		this.centeringNumberOfDay();
+		this.createGraph();
+		this.createListeners();
 	}
 }
